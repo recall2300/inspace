afeb5c2245466cf8059d6d4800323b3ac9727a76
woohyeok <tororum4992@gmail.com>
2016-12-29 16:34:40 +0900

    Add Redux but Something wrong...:-(

--- inspace/urls.py
+++ inspace/urls.py
@@ -26,7 +26,7 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    # url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
     url(r'^view1/', generic.TemplateView.as_view(template_name='view1.html')),
--- reactjs/containers/App1Container.jsx
+++ reactjs/containers/App1Container.jsx
@@ -1,14 +1,29 @@
 import React from "react"
 
+import {connect} from "react-redux"
+
+import * as counterActions from "../actions/counterActions"
 import Headline from "../components/Headline"
 
-export default class App1Container extends React.Component {
+@connect(state => ({
+    counters: state.counters,
+}))
+export default class SampleAppContainer extends React.Component {
+    handleClick() {
+        let {dispatch} = this.props;
+        dispatch(counterActions.increaseCounter())
+    }
+
     render() {
+        let {counters} = this.props
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-sm-12">
-                        <Headline>Something New!!asdasd</Headline>
+                        <Headline>Sample App!</Headline>
+                        <div onClick={() => this.handleClick()}>INCREASE</div>
+                        <p>{counters.clicks}</p>
+                        <p>{process.env.BASE_API_URL}</p>
                     </div>
                 </div>
             </div>
