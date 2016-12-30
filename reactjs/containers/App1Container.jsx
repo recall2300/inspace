c7098f0d75e167e50fce7b1b56b570de0775ddb0
woohyeok <tororum4992@gmail.com>
2016-12-30 10:26:07 +0900

    Complete ReactJS with Django Project Tutorial

--- reactjs/containers/App1Container.jsx
+++ reactjs/containers/App1Container.jsx
@@ -1,29 +1,70 @@
 import React from "react"
+import Radium from "radium"
 
 import {connect} from "react-redux"
 
 import * as counterActions from "../actions/counterActions"
+import * as githubActions from "../actions/githubActions"
 import Headline from "../components/Headline"
+import GithubRepos from "../components/GithubRepos"
+
+const styles = {
+    button: {
+        cursor: "pointer",
+    },
+    counter: {
+        color: "blue",
+        fontSize: "20px",
+    }
+}
 
 @connect(state => ({
     counters: state.counters,
+    github: state.github,
 }))
-export default class SampleAppContainer extends React.Component {
+
+@Radium
+export default class App1Container extends React.Component {
     handleClick() {
         let {dispatch} = this.props;
         dispatch(counterActions.increaseCounter())
     }
 
+    componentDidMount() {
+        let {dispatch, github} = this.props
+        if (!github.isLoadingRepos && github.repos === undefined) {
+            dispatch(githubActions.fetchRepos())
+        }
+    }
+
+    renderLoading() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        Loading...
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
-        let {counters} = this.props
+        let {counters, github} = this.props
+        if (github.isLoadingRepos || github.repos === undefined) {
+            return this.renderLoading()
+        }
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-sm-12">
                         <Headline>Sample App!</Headline>
-                        <div onClick={() => this.handleClick()}>INCREASE</div>
-                        <p>{counters.clicks}</p>
+                        <div style={[styles.button]} onClick={() => this.handleClick()}>INCREASE</div>
+                        <p style={[styles.counter]}>{counters.clicks}</p>
                         <p>{process.env.BASE_API_URL}</p>
+                        {github.repos !== undefined &&
+                            <GithubRepos repos={github.repos}/>
+                        }
                     </div>
                 </div>
             </div>
