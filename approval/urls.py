2fa050fb6d542d02e3125d9a17a810bb8cc10dce
woohyeok <tororum4992@gmail.com>
2017-01-11 18:28:30 +0900

    ADD Login, Side Menu EDIT Main template

--- approval/urls.py
+++ approval/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.approval_list, name="approval_list"),
-    url(r'^login$', views.approval_login, name="approval_logint"),
+    url(r'^login$', views.approval_login, name="approval_login"),
+    url(r'^login_do$', views.approval_login_do, name="approval_login_do"),
+    url(r'^logout$', views.approval_logout, name="approval_logout"),
     url(r'^approval/(?P<pk>[0-9]+)/$', views.approval_detail, name="approval_detail"),
     url(r'^approval/new/$', views.approval_new, name='approval_new'),
     url(r'^approval/(?P<pk>[0-9]+)/edit/$', views.approval_edit, name='approval_edit'),
--- approval/views.py
+++ approval/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User, Group
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 from rest_framework import viewsets
 from approval.serializers import UserSerializer, GroupSerializer
 from .models import Approval
@@ -20,19 +22,43 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+
 def approval_list(request):
-    approvals = Approval.objects.all()
-    return render(request, "approval/approval_list.html", {'approvals':approvals})
+    if request.user.is_authenticated:
+        approvals = Approval.objects.all()
+        return render(request, "approval/approval_list.html", {'approvals': approvals})
+    else:
+        return redirect('approval_login')
+
 
 def approval_login(request):
-    return render(request, "approval/approval_login.html")
+    if request.user.is_authenticated:
+        return redirect('approval_list')
+    else:
+        return render(request, "approval/approval_login.html")
+
+def approval_login_do(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('approval_list')
+    else:
+        # Return an 'invalid login' error message.
+        return redirect('approval_login')
+
+def approval_logout(request):
+    logout(request)
+    return redirect('approval_login')
 
 def approval_detail(request):
     return
 
+
 def approval_new(request):
     return
 
+
 def approval_edit(request):
     return
-
