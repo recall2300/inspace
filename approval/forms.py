fee2c2fdab864d5843a54be8beae272f8b6ae3c0
donook <recall2300@gmail.com>
2017-01-12 18:30:32 +0900

    fixed 'employee' user custom model.

--- approval/forms.py
+++ approval/forms.py
@@ -2,7 +2,10 @@ from django import forms
 
 from .models import Approval
 
+
 class ApprovalForm(forms.ModelForm):
     class Meta:
         model = Approval
-        fields = ('name','reason',)
+        fields = (
+        'department', 'position', 'name', 'reason', 'start_date', 'end_date', 'write_date', 'leave_classification',
+        'emergency_contact', 'destination', 'comments')
